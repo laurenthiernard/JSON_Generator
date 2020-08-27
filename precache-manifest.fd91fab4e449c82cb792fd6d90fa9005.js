dff1b47efaabb7d01fb3af69c93654443f93da7c
laurenthiernard <laurent.hiernard@gmail.com>
2020-08-28 09:50:18 +1000

    updates

---
+++ precache-manifest.fd91fab4e449c82cb792fd6d90fa9005.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e0d8cf15f13a84e93895cc0634aa3f1",
+    "url": "/JSON_Generator/index.html"
+  },
+  {
+    "revision": "c70772a564824e6a9cc4",
+    "url": "/JSON_Generator/static/css/main.0c41d2c1.chunk.css"
+  },
+  {
+    "revision": "968bfeaec19b861b3f1d",
+    "url": "/JSON_Generator/static/js/2.741e01b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JSON_Generator/static/js/2.741e01b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c70772a564824e6a9cc4",
+    "url": "/JSON_Generator/static/js/main.1d9ba42f.chunk.js"
+  },
+  {
+    "revision": "3d725e5e4765aca3e7e4",
+    "url": "/JSON_Generator/static/js/runtime-main.04b0f54b.js"
+  },
+  {
+    "revision": "94cc27a516608b11f99795e2ff41d8e4",
+    "url": "/JSON_Generator/static/media/bg.94cc27a5.jpg"
+  }
+]);
